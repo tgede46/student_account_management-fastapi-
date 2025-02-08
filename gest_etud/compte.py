b8a6fcd3da3d3ec5e01a4070aa35295a3854bc8c
Gedeonkp <152423853+tgede46@users.noreply.github.com>
2025-02-08 10:18:55 +0000

    Add files via upload

---
+++ gest_etud/compte.py
@@ -0,0 +1,15 @@
+class Compte:
+    def __init__(self,id_client,client,solde,historique_transaction):
+        self.id_client=id_client
+        self.client=client
+        self.solde=solde
+        self.transaction=historique_transaction
+
+    def depot(montant):
+
+
+    def retrait(montant):
+
+
+    def transfert(montant,compte_destinataire):
+        
